42d617a307fa7da7d5beeced277ff3d2deb6059a
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2025-03-27 10:02:57 +0100

    Update clearDisplay function. Create giveResult function
    The giveResult function checks if the numbers/operators variables are not empty and call the operate function giving the result and showing it in the display

--- calculator.js
+++ calculator.js
@@ -1,11 +1,12 @@
-let n1 = "";
-let n2 = "";
-let operator = "";
+let n1 = "4";
+let n2 = "3";
+let operator = "+";
 let displayValue = "";
 
 const calculatorContainer = document.querySelector(".calculatorContainer");
 const numbersContainer = document.querySelector(".numbersContainer");
 const operatorsContainer = document.querySelector(".operatorsContainer");
+const otherBtnContainer = document.querySelector(".otherBtnContainer");
 let display = document.querySelector(".display");
 
 
@@ -33,7 +34,7 @@ function exponential(n1, n2) {
     return n1 ** n2;
 };
 
-function operate(n1, n2, operator) {
+function operate(n1, operator, n2) {
     // Use switch instead of if/else
     switch (operator) {
         case "+":
@@ -53,44 +54,39 @@ function operate(n1, n2, operator) {
 
 //console.log(operate(n1, n2, operator));
 
-
-calculatorContainer.addEventListener("click", e => {
-    if (e.target.matches(".btnN") && operator !== "") {
-        n2 += parseInt(e.target.textContent);
-        displayValue = n1 + operator + n2;
-        updateDisplay();
-    }
-
-    else if (e.target.matches(".btnO")) {
-        operator = e.target.textContent;
-        displayValue = n1 + operator;
-        updateDisplay();
-    }
-
-    else {
-        n1 += parseInt(e.target.textContent);
-        displayValue = n1;
-        updateDisplay();
-    };
-
-});
-
-
-function updateDisplay() { display.textContent = displayValue }
-
-
+// Resetta tutte le variabili svuotandole dei valori precedentemente inseriti
 function clearDisplay(type, CssSelector, callbackF) {
-    calculatorContainer.addEventListener(type, e => {
+    otherBtnContainer.addEventListener(type, e => {
         if (e.target.matches(CssSelector)) {
-            callbackF()
-        }
+            callbackF();
+        };
     });
 };
-
 
 clearDisplay("click", ".clear", () => {
     n1 = "";
     n2 = "";
     operator = "";
-    display.textContent = "0.0"
+    displayValue = "0.0";
+    display.textContent = displayValue;
 });
+
+// Prende le variabili n1 n2 operator e chiama la funzione operate dandone il risulato
+function giveResult(type, CssSelector, callbackF) {
+    otherBtnContainer.addEventListener(type, e => {
+        if (e.target.matches(CssSelector)) {
+            callbackF();
+        };
+    });
+};
+
+giveResult("click", ".result", () => {
+    if (n1 !== "" && n2 !== "" & operator !== "") {
+        let result = operate(parseInt(n1), operator, parseInt(n2));
+        displayValue = result;
+        display.textContent = displayValue;
+    };
+});
+
+// Creare una funzione getNumbers
+// Creare una funzione getOperators
